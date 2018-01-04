536bc617b15709de702c59bc3faad2f9316ca2be
Brendan Gregory <brendangregory@Brendans-MacBook-Pro.local>
2018-01-04 01:04:21 -0500

    Initial Commit

---
+++ king-rule-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  king-rule-cpp
+//
+//  Created by Brendan Gregory on 1/4/18.
+//  Copyright © 2018 Brendan Gregory. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
